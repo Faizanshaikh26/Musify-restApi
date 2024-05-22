7d0463f9a1b30d8636a1c137216b2038d5f780d4
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-05-22 11:01:02 +0530

    imd deleted

--- config.js
+++ config.js
@@ -3,3 +3,85 @@
 const hostedUrl = "https://musify-api.onrender.com"; // Your hosted URL
 
 module.exports = { hostedUrl };
+
+
+
+// {
+//     "title": "Trending ",
+//     "description": "#1 on Trending ",
+//     "genre": "Varoius",
+//     "albumImage": "https://ex/1.jpg",
+//     "songs": [
+//       {
+//         "title": "Feelings",
+//         "artist": "Sumit Goswami",
+//         "duration": 206.4,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Feelings.mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/feeling.jpeg"
+//       },
+//       {
+//         "title": "Genda Phool",
+//         "artist": "Badshah",
+//         "duration": 150,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Genda Phool (feat. Payal Dev).mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/Gednda phool.jpeg"
+//       },
+//       {
+//         "title": "Haan Main Galat",
+//         "artist": "Pritam & Arijit ",
+//         "duration": 202.8,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Haan Main Galat (From _Love Aaj Kal_).mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/Haan mai galat.jpeg"
+//       },
+//       {
+//         "title": "Main Tumhara",
+//         "artist": "A.R Rahman",
+//         "duration": 250.8,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Main Tumhara (From _Dil Bechara_).mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/husn.jpeg"
+//       },
+//       {
+//         "title": "Mehrama",
+//         "artist": "Pritam & Darshan ",
+//         "duration": 245.4,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Mehrama (From _Love Aaj Kal_).mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/Mehrama.jpeg"
+//       },
+//       {
+//         "title": "Phir Chala",
+//         "artist": "Payal & Jubin",
+//         "duration": 256.6,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Phir Chala (From _Ginny Weds Sunny_).mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/Phir chala.jpeg"
+//       },
+//       {
+//         "title": "Sawan Mein Lag Gayi Aag",
+//         "artist": "Mika & Payal",
+//         "duration": 207.6,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Sawan Mein Lag Gayi Aag (From _Ginny Weds Sunny_).mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/Sawan m lag gye aag.jpeg"
+//       },
+//       {
+//         "title": "Shayad",
+//         "artist": "Pritam & Arijit ",
+//         "duration": 244.2,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Shayad (From _Love Aaj Kal_).mp3",
+//         "songImage":"https://musify-restapi.onrender.com/images/Trending_img/shayd.jpeg"
+//       },
+//       {
+//         "title": "Tu Mera Nahi",
+//         "artist": "Amaal Malik",
+//         "duration": 210,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Tu Mera Nahi.mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/tu mera nahi.jpeg"
+//       },
+//       {
+//         "title": "Tum Se Hi",
+//         "artist": "Ankit & Leena",
+//         "duration": 265.8,
+//         "songUrl": "https://musify-restapi.onrender.com/songurl/Trending/Tum Se Hi (From _Sadak 2_).mp3",
+//         "songImage": "https://musify-restapi.onrender.com/images/Trending_img/tum se he.jpeg"
+//       }
+//     ]
+//   }
+  
